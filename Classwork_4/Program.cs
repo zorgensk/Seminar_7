203a874a4c21a7058a00ccd4b7f58267cef7bae5
zorgensk <zorgensk@gmail.com>
2022-12-05 21:44:25 +0700

    Classwork_4

---
+++ Classwork_4/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов
+// находящихся на главной диагонали.
+
+int[,] Random2Array(int m, int n) 
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+void Print2Array(int[,] arr) // Вывод двумерного массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(($"{arr[i, j]} "));
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк двумерного массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов двумерного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[,] array = Random2Array(m, n);
+Print2Array(array);
+int sum = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (i == j)
+             sum += array[i,j];    
+    }
+}
+Console.WriteLine($"Сумма элементов = {sum}");
